cbcf7d704cef2cbad81e1606b162cd8b5cd4a7c3
Anusha-Jagannath <anushajaganath6@gmail.com>
2023-10-01 00:09:32 +0530

    Added code for coroutines

---
+++ src/coroutines/Main1.kt
@@ -0,0 +1,20 @@
+package coroutines
+
+import kotlin.concurrent.thread
+
+
+fun main(args: Array<String>) {
+    println("Learning coroutines")
+    println("Main program starts here ${Thread.currentThread().name}")
+
+
+    thread {
+        println("The fake work starts here ${Thread.currentThread().name}")
+        Thread.sleep(1000)
+        println("The fake work ends here ${Thread.currentThread().name}")
+    }
+
+    println("Main program ends here ${Thread.currentThread().name}")
+
+
+}
---
+++ src/variables/Main.kt
@@ -0,0 +1,15 @@
+package variables
+
+
+fun main(args: Array<String>) {
+
+    var myNumber = 10
+    var myDecimal = 1.0
+
+    println(myNumber)
+    println(myDecimal)
+
+    var myString = "Hello world"
+    println(myString)
+
+}
