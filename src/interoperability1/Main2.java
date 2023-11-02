8070523f198800036e01cb307119feec0abab050
Anusha-Jagannath <anushajaganath6@gmail.com>
2023-11-03 00:12:35 +0530

    Added code for interoperability

---
+++ src/interoperability1/Main2.java
@@ -0,0 +1,13 @@
+package interoperability1;
+
+public class Main2 {
+
+    public static void main(String args[]) {
+        System.out.println("Learning interoperability");
+        System.out.println(MainKt.add(2, 3));
+    }
+
+    public int findSum(int a, int b) {
+        return a + b;
+    }
+}
